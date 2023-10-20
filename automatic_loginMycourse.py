2d602eaf6fd9411c1675080aaa4f1022c85f36b8
Sevegoth <144342588+Sevegoth@users.noreply.github.com>
2023-10-20 13:31:38 +0700

    Update automatic_loginMycourse.py

--- automatic_loginMycourse.py
+++ automatic_loginMycourse.py
@@ -3,8 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import *
 from selenium.webdriver.support import expected_conditions as EC
 
-# "E:\Programming_Language\Python\Lib\site-packages\chromedriver.exe"
-
 driver = webdriver.Chrome()
 
 driver.get("https://mycourses.ict.mahidol.ac.th/login/index.php")
@@ -17,9 +15,10 @@ username_input = driver.find_element(By.NAME,"UserName")
 password_input = driver.find_element(By.NAME,"Password")
 login_button = wait.until(EC.presence_of_element_located((By.ID,"submitButton")))
 
-username_input.send_keys("u6488154")
-password_input.send_keys("gzmtgzs52nm")
+username_input.send_keys("your username")
+password_input.send_keys("your password")
 login_button.click()
 
-while(0==0):
-    continue
+# In case it will close the window, using the code below to keep the window activing
+# while(0==0):
+#    continue
